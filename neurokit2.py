de99f74c9e9b33693cf26b339c86b5f1a76434e6
mxsp <166367334+mxsp@users.noreply.github.com>
2024-04-08 09:11:26 +0200

    Update neurokit2.py

--- neurokit2.py
+++ neurokit2.py
@@ -8,3 +8,15 @@ processed_data, info = nk.bio_process(ecg=data["ECG"], rsp=data["RSP"], eda=data
 
 # Compute relevant features
 results = nk.bio_analyze(processed_data, sampling_rate=100)
+
+
+import neurokit2 as nk
+
+# Generate 15 seconds of ECG signal (recorded at 250 samples/second)
+ecg = nk.ecg_simulate(duration=15, sampling_rate=1000, heart_rate=70)
+
+# Process it
+signals, info = nk.ecg_process(ecg, sampling_rate=1000)
+
+# Visualise the processing
+nk.ecg_plot(signals, info)
